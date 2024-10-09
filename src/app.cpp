5c3fc09cf3dc0d81f132fbaafde676eac646455a
Duane Northcutt <duane.northcutt@gmail.com>
2024-10-09 13:33:37 -0700

    Updated.

--- src/app.cpp
+++ src/app.cpp
@@ -10,9 +10,9 @@
 #include "leds.hpp"
 
 namespace smartspeaker {
-  App::App() { printf("APP Create"); };
+  App::App() { printf("APP Create"); }
 
-  App::~App() { printf("APP Remove"); };
+  App::~App() { printf("APP Remove"); }
 
   int smartspeaker::App::exec(int argc, char *argv[]) {
     config = std::make_unique<Config>();
@@ -20,6 +20,11 @@ namespace smartspeaker {
 
     leds = std::make_unique<Leds>(this);
     leds->init();
+    printf("Starting\n");
     leds->animate(LedsState_t::Starting);
-  };
-};
+    printf("Listening\n");
+    leds->animate(LedsState_t::Listening);
+    printf("Processing\n");
+    leds->animate(LedsState_t::Processing);
+  }
+}
