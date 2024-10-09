a6a0aacb6a3b8255a49d9f6a2eda9f095e83a285
Duane Northcutt <duane.northcutt@gmail.com>
2024-10-09 13:33:52 -0700

    Updated.

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,7 @@ int main(int argc, char *argv[]) {
   using namespace smartspeaker;
   printf("SmartSpeaker App\n");
   App app;
+  printf("...\n");
   return app.exec(argc, argv);
+  printf("DONE");
 }
