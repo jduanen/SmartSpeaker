8afa7e8e59bb3e88ad9db01b757729cf6a783f27
Duane Northcutt <duane.northcutt@gmail.com>
2024-10-22 14:01:53 -0700

    Added delays between startup states.

--- src/app.cpp
+++ src/app.cpp
@@ -4,6 +4,7 @@
 
 #include <glib-unix.h>
 #include <glib.h>
+#include <unistd.h>
 
 #include "config.hpp"
 #include "app.hpp"
@@ -16,17 +17,23 @@ namespace smartspeaker {
 
   int smartspeaker::App::exec(int argc, char *argv[]) {
     config = std::make_unique<Config>();
-//    config->leds_enabled = true;
     config->load();
+
+    printf("LEDs enabled: %d\n", config->leds_enabled);
 
     leds = std::make_unique<Leds>(this);
     leds->init();
     printf("Starting\n");
     leds->animate(LedsState_t::Starting);
+    usleep(1000000);  // Delay for 1 second (1,000,000 microseconds)
+
     printf("Listening\n");
     leds->animate(LedsState_t::Listening);
+    usleep(1000000);  // Delay for 1 second (1,000,000 microseconds)
+
     printf("Processing\n");
     leds->animate(LedsState_t::Processing);
+    usleep(1000000);  // Delay for 1 second (1,000,000 microseconds)
     return 0;
   }
 }
