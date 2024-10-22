0b7a25c6365ddb26911f45efe3da382dd39b5d42
Duane Northcutt <duane.northcutt@gmail.com>
2024-10-22 10:45:06 -0700

    Updated.

--- armBuild.sh
+++ armBuild.sh
@@ -3,46 +3,13 @@
 # install build stuff
 sudo apt-get update && sudo apt-get install -y \
     build-essential \
-    wget \
-    curl \
-    git \
     cmake \
-    meson
+    meson \
+    libffi-dev \
+    gettext \
+    libmount libmount-dev \
+    libglib2.0-dev \
+    adb
 
-# N.B.
-#  * libglib-2.0.so
-#    - /usr/lib/libglib-2.0.so.0.5000.1 (TinaLinux)
-#    - libglib-2.0.so.0.6600.8
-#  * TinaLinux
-#    - /lib/libc.so.6: GLIBC_2.23
-#    - 
-
-# install glib-2
-cd lib
-wget https://download.gnome.org/sources/glib/2.58/glib-2.58.3.tar.xz
-tar xf glib-2.58.3.tar.xz
-cd glib-2.58.3/
-mkdir builddir
-cd builddir
-meson setup --default-library=static ..
-ninja
-
-adb push ./glib/libglib-2.0.a /opt/ss/lib/
-
-# get sources
-git clone git@github.com:jduanen/SmartSpeaker.git
-
-# build application
-cd SmartSpeaker
-meson setup -Dprefix=/usr/lib/arm-linux-gnueabihf -Dlibdir=lib --default-library=static build
-
-ninja -C build
-
-adb push build/src/smartspeaker /opt/ss/bin/
-
-adb push config.ini /opt/ss/
-
-
-# N.B.
-# * on target:
-#   - export LD_LIBRARY_PATH=/opt/ss/libs
+mkdir ~/Code
+cd ~/Code
